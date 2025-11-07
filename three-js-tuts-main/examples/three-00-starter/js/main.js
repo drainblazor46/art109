24bc5618376a2b7d235197207afe0d6def412fd7
Joshua Chan <soulhexcalibur101@gmail.com>
2025-11-07 15:06:19 -0800

    adding code to main.js and adding three.js library to

--- three-js-tuts-main/examples/three-00-starter/js/main.js
+++ three-js-tuts-main/examples/three-00-starter/js/main.js
@@ -3,7 +3,7 @@
 
 
 //~~~~~~~Import Three.js (also linked to as import map in HTML)~~~~~~
-import * as THREE from 'three';
+import * as THREE from '/three/build/three.module.js';
 
 // Import add-ons
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
@@ -11,14 +11,51 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 // ~~~~~~~~~~~~~~~~Create scene here~~~~~~~~~~~~~~~~
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+let scene, camera, renderer, cube;
+
+function init() {
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera
+(75, 
+    window.innerWidth / window.innerHeight, 
+    0.1, 
+    1000);
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
 
+const texture = new THREE.TextureLoader().load('textures/crate.gif');
+const material = new THREE.MeshBasicMaterial( { map: texture } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5; 
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setsize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
+animate();
 
 // ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
 const controls = new OrbitControls(camera, renderer.domElement);
