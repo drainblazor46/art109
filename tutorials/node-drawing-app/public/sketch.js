566df5045b7d64f80714bfdaba80dde2accef055
Joshua Chan <soulhexcalibur101@gmail.com>
2025-11-14 20:11:11 -0800

    Tut 10: node.js drawing app update

---
+++ tutorials/node-drawing-app/public/sketch.js
@@ -0,0 +1,32 @@
+var socket;
+
+function setup() {
+    createCanvas(800, 800);
+    background(0);
+
+    socket = io.connect('http://localhost:3000');
+    socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data){
+    noStroke();
+    fill(255, 0, 100);
+    ellipse(data.x, data.y, 8, 8);
+}
+
+function mouseDragged() {
+    console.log('Sending: ' + mouseX + ',' + mouseY);
+
+    var data = {
+        x: mouseX,
+        y: mouseY
+    }
+    socket.emit('mouse', data);
+
+    noStroke();
+    fill(255);
+    ellipse(mouseX, mouseY, 8, 8);
+}
+function draw() {
+
+}
